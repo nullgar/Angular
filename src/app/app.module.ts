6c6286a2fee7aea2e361eade0dd235b28eb5aa03
Jon Aguilar <technicianaguilar@gmail.com>
2023-02-22 21:18:10 -0800

    Created a basic Star Component and stated creating a Event Emitter for when the start are clicked

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
+import { StarComponent } from './products/star/star.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProductListComponent,
-    ConvertToSpacesPipe
+    ConvertToSpacesPipe,
+    StarComponent,
   ],
   imports: [
     BrowserModule,

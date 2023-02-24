f143bc05973814f2de133fa6fb063dd71de6ee39
Jon Aguilar <technicianaguilar@gmail.com>
2023-02-23 22:22:51 -0800

    Finished subscription service and created an onDestroy function

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -1,17 +1,20 @@
 import { IProduct } from './products';
-import { Component, OnInit } from "@angular/core";
+import { Component, OnDestroy, OnInit } from "@angular/core";
 import { ProductService } from './product.service';
+import { Subscription } from 'rxjs';
 
 @Component({
     selector: 'pm-products',
     templateUrl: './product-list.component.html',
     styleUrls: ['./product-list.component.css']
 })
-export class ProductListComponent implements OnInit {
+export class ProductListComponent implements OnInit, OnDestroy {
     pageTitle: string = 'Product List';
     imageWidth: number = 50;
     imageMargin: number = 2;
     showImage: boolean = false;
+    errorMessage: string = '';
+    sub!: Subscription;
 
     private _listFilter: string = '';
 
@@ -44,9 +47,19 @@ export class ProductListComponent implements OnInit {
 
     ngOnInit(): void {
       this.listFilter = "";
-      this.products = this.productService.getProducts();
-      this.filteredProducts = this.products;
+
+      this.sub = this.productService.getProducts().subscribe({
+        next: products => {
+          this.products = products
+          this.filteredProducts = this.products;
+        },
+        error: err => this.errorMessage = err
+      });
     };
+
+    ngOnDestroy(): void {
+      this.sub.unsubscribe();
+    }
 
     onRatingClicked(message: string): void {
       this.pageTitle = `Product List: ${message}`;

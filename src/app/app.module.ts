ef4e62ee4982ec0e668960b919c1b2c73c1cc24f
Jon Aguilar <technicianaguilar@gmail.com>
2023-02-27 22:33:29 -0800

    Created the product details component and added the required import statements

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { WelcomeComponent } from './home/welcome.component';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
@@ -7,13 +8,16 @@ import { AppComponent } from './app.component';
 import { StarComponent } from './products/star/star.component';
 import { ConvertToSpacesPipe } from './shared/convert-to-space.pipe';
 import { ProductListComponent } from './products/product-list.component';
+import { ProductDetailComponent } from './products/product-detail.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    WelcomeComponent,
     ProductListComponent,
     ConvertToSpacesPipe,
     StarComponent,
+    ProductDetailComponent,
   ],
   imports: [
     BrowserModule,
